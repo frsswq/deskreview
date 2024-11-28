cf6290dcba6badb2f1bebf7f34a35ed634d8456a
frsswq <farissaifuddin.work@gmail.com>
2024-11-28 21:23:33 +0700

    fix file routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://frsswq.github.io',
-  base: '/deskreview/',
+  base: 'deskreview',
   integrations: [tailwind()]
 });
