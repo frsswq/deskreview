ae091083bf33fb1db0b488a7e24ba7713a30d82d
frsswq <farissaifuddin.work@gmail.com>
2024-11-30 02:46:14 +0700

    rewrite cardData to typescript and add placeholder image

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -1,48 +1,56 @@
----
-import astra_honda from "../assets/astra_honda_red.png";
-import rococo from "../assets/rococo.png";
-import hayati_karya from "../assets/hayati_logo.png";
+import placeholderImg from "../assets/placeholder.png";
+import astraHondaImg from "../assets/astra_honda_red.png";
+import rococoImg from "../assets/rococo.png";
+import hayatiImg from "../assets/hayati_logo.png";
 
-export const cardData = [
+type CardData = {
+  title: string;
+  description: string;
+  image: ImageMetadata;
+  year: string;
+  modalDescription: string;
+  modalServices: string;
+};
+
+export const cardData: CardData[] = [
   {
     title: "Salam Ganesha Academy",
     description: "Education Tech",
-    image: "/",
+    image: placeholderImg,
     year: "2024",
     modalDescription: "As an education startup, Salam Ganesha Academy transitioned to targeting a more mature senior executive market. Several digital strategies were executed to ensure a smooth transition.",
-    modalServices: "campaign strategy & development, social media development, kol management"
+    modalServices: "campaign strategy & development, social media development, kol management",
   },
   {
     title: "Telkomsel",
     description: "Telecommunication",
-    image: "/",
+    image: placeholderImg,
     year: "2023",
     modalDescription: "After their merger with IndiHome, Telkomsel aimed to create a nationwide corporate messaging campaign under #BersatuLebihMaju, resulting in +1 million impression, 20% increase of Engagement Rate, and more than 3000 followers.",
-    modalServices: "campaign strategy & development, social media strategy, online activation development"
+    modalServices: "campaign strategy & development, social media strategy, online activation development",
   },
   {
     title: "Astra Honda",
     description: "Vehicle Manufacture",
-    image: astra_honda,
+    image: astraHondaImg,
     year: "2023",
     modalDescription: "As their effort to gain trust with local wisdom and the spirit of #OneHeart, Honda aims to reach more trust and connection withing Indonesia market.",
-    modalServices: "Content research & analytic, digital trend analytic"
+    modalServices: "Content research & analytic, digital trend analytic",
   },
   {
     title: "Rococo Group",
     description: "Luxury Fashion Retail",
-    image: rococo,
+    image: rococoImg,
     year: "2023",
     modalDescription: "Rococo Group Indonesia is a luxury fashion retailer established in 2000, focusing on high-end fashion brands with partners from Italy, UK, and Hongkong.",
-    modalServices: "campaign strategy & development, social media development, kol management"
+    modalServices: "campaign strategy & development, social media development, kol management",
   },
   {
     title: "Hayati Karya",
     description: "Carbon Trading",
-    image: hayati_karya,
+    image: hayatiImg,
     year: "2023",
-    modalDescription: "Hayati Karya Lestari is a global carbon trading corporation with various business line, launching in Indonesia with the vision to create sustainable future for Indonesia.",
-    modalServices: "Branding strategy, social media & website, brand architecture"
-  }
+    modalDescription: "Hayati Karya Lestari is a global carbon trading corporation with various business line, launching in Indonesia with the vision to create sustainable future for Indonesia.", 
+    modalServices: "Branding strategy, social media & website, brand architecture",
+  },
 ];
----
