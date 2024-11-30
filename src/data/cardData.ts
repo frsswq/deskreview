816329f016460bf1669cae9e2d9eee1473c5fcdb
frsswq <farissaifuddin.work@gmail.com>
2024-11-30 23:01:19 +0700

    im going insane

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -1,8 +1,8 @@
 import placeholderImg from "../assets/placeholder.png";
-import astraHondaImg from "../assets/astra_honda_red.png";
-import rococoImg from "../assets/rococo.png";
-import hayatiImg from "../assets/hayati_logo.png";
-import ganeshaImg from "../assets/salam_ganesha.png";
+import astraHondaImg from "../assets/astra.png";
+
+import hayatiImg from "../assets/hayati.png";
+
 import telkomselImg from "../assets/telkomsel.png";
 
 type CardData = {
@@ -18,18 +18,10 @@ export const cardData: CardData[] = [
   {
     title: "Salam Ganesha Academy",
     description: "Education Tech",
-    image: ganeshaImg,
+    image: placeholderImg,
     year: "2024",
     modalDescription: "As an education startup, Salam Ganesha Academy transitioned to targeting a more mature senior executive market. Several digital strategies were executed to ensure a smooth transition.",
     modalServices: "campaign strategy & development, social media development, kol management",
-  },
-  {
-    title: "Telkomsel",
-    description: "Telecommunication",
-    image: telkomselImg,
-    year: "2023",
-    modalDescription: "After their merger with IndiHome, Telkomsel aimed to create a nationwide corporate messaging campaign under #BersatuLebihMaju, resulting in +1 million impression, 20% increase of Engagement Rate, and more than 3000 followers.",
-    modalServices: "campaign strategy & development, social media strategy, online activation development",
   },
   {
     title: "Astra Honda",
@@ -40,9 +32,17 @@ export const cardData: CardData[] = [
     modalServices: "Content research & analytic, digital trend analytic",
   },
   {
+    title: "Telkomsel",
+    description: "Telecommunication",
+    image: telkomselImg,
+    year: "2023",
+    modalDescription: "After their merger with IndiHome, Telkomsel aimed to create a nationwide corporate messaging campaign under #BersatuLebihMaju, resulting in +1 million impression, 20% increase of Engagement Rate, and more than 3000 followers.",
+    modalServices: "campaign strategy & development, social media strategy, online activation development",
+  },
+  {
     title: "Rococo Group",
     description: "Luxury Fashion Retail",
-    image: rococoImg,
+    image: placeholderImg,
     year: "2023",
     modalDescription: "Rococo Group Indonesia is a luxury fashion retailer established in 2000, focusing on high-end fashion brands with partners from Italy, UK, and Hongkong.",
     modalServices: "campaign strategy & development, social media development, kol management",
