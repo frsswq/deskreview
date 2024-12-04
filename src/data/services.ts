c9aba0effe9d6d7538d90f4702f4f9944b84362b
frsswq <farissaifuddin.work@gmail.com>
2024-12-04 11:19:05 +0700

    reduce gif size, try preloading fonts.

---
+++ src/data/services.ts
@@ -0,0 +1,24 @@
+type Services = {
+  title: string;
+  types: string[];
+};
+
+export const services: Services[] = [
+  {
+    title: "Digital Campaign",
+    types: [
+      "Social media handling",
+      "Digital campaign set-up",
+      "Digital ads handling",
+      "Digital trend analysis and research",
+    ],
+  },
+  {
+    title: "Brand Strategy",
+    types: [
+      "Visual identity development",
+      "Branding communication set-up",
+      "Branding implementation set-up",
+    ],
+  },
+];
