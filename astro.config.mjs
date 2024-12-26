ecdf15ddfdd9086a15f6345f697d81159563ad77
frsswq <farissaifuddin.work@gmail.com>
2024-12-26 19:58:13 +0700

    make word modal look like modal

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://frsswq.github.io',
   base: 'deskreview',
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), mdx()],
   trailingSlash: 'never',
 });
